052e0e62f2b3687f4e0e791d17c04c40301fa1a3
chotiwich <chotiwich6793@gmail.com>
2020-05-05 19:48:11 +0700

    แก้ไข ปรับแต่งหน้า detail ใหม่

--- application/views/detail/content.php
+++ application/views/detail/content.php
@@ -3,86 +3,63 @@
         <div class="row">
             <div class="col-md-4">
                 <img src="<?php echo base_url('public/images/staffs/staff02.jpg') ?>" class="img-fluid w-50 mx-auto d-block" />
-                <h6 class="text-center bg-danger text-white">X-Men: The Last Stand (2006) </h6>
-                <h3>X-MEN 3 The Last Stand (2006) รวมพลังประจัญบาน</h3>
-                <table class="table table-borderless">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <h6>144 </h6>
-                            </td>
-                            <td>
-                                <h6> mins</h6>
-                            </td>
-                            <td>
-                                <h6> </h6>
-                            </td>
-                            <td>
-                                <h6>Rating : </h6>
-                            </td>
-                            <td>
-                                <h6>4/5</h6>
-                            </td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                <h5 class="text-center bg-danger text-white">X-Men: The Last Stand (2006) </h5>
+                <h2 class="text-white">X-MEN 3 The Last Stand (2006) รวมพลังประจัญบาน</h2>
+                <hr>
+                
+            <div class="row">
+                <div class="col-md-4">   
+                    <h4 class="text-white">ความยาวหนัง : </h4>
+                </div>
+                <div class="col-md-3">   
+                    <h4 class="text-white">144 mins</h4>
+                </div>
+            </div>
 
-                    </tbody>
-                    <table class="table table-borderless"></table>
-                    <tbody>
+            <div class="row">
+                <div class="col-md-4">   
+                    <h4 class="text-white">หมวดหมู่ : </h4>
+                </div>
+                <div class="col-md-3">   
+                    <h4 class="text-white">ยอดนิยม</h4>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">   
+                    <h4 class="text-white">ผู้กำกับ :</h4>
+                </div>
+                <div class="col-md-4">   
+                    <h4 class="text-white">เบร็ต แรตเนอร์</h4>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">   
+                    <h4 class="text-white">ปีที่ออกฉาย :</h4>
+                </div>
+                <div class="col-md-3">   
+                    <h4 class="text-white">2006</h4>
+                </div>
+            </div>
+
+                
+
+                    
+            <table class="table table-borderless"></table>
+                <tbody>
                         <tr>
-                            <td><button type="button" class="btn btn-outline-danger btn-block">Watch movie</button></td>
-                            <td><button type="button" class="btn btn-outline-danger btn-block">Back</button></td>
+                            <td><button type="button" class="btn-danger btn-block">Watch movie</button></td>
+                            <td><button type="button" class="btn-danger btn-block">Back</button></td>
                         </tr>
                 </table>
-                </table>
+            </table>
             </div>
 
             <div class="col-md-8">
-                <iframe width="750" height="400" src="https://www.youtube.com/embed/gT2nVJTSPSc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-                <table class="table table-borderless">
-
-                    <tbody>
-                        <tr>
-                            <td>
-                                <h6>หมวดหมู่ :</h6>
-                            </td>
-                            <td>
-                                <h6>หนังต่างประเทศ</h6>
-                            </td>
-                            <td>
-                                <h6>ความยาวหนัง :</h6>
-                            </td>
-                            <td>
-                                <h6>144 นาที</h6>
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <h6> ผู้กำกับ : </h6>
-                            </td>
-                            <td>
-                                <h6>เบร็ต แรตเนอร์</h6>
-                            </td>
-                            <td>
-                                <p> ปีที่ออกฉาย : </p>
-                            </td>
-                            <td>
-                                <p>2006</p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <p>บทส่งท้ายไตรภาคของ X-MEN จะเป็นองก์ เยียวยา ที่พอเห็นทางลดทอนภัยจากมนุษย์กลายพันธุ์ที่หวังจะพลิกโฉมหน้าประวัติศาสตร์ เป็นครั้งแรกที่เหล่ามนุษย์กลายพันธุ์จะได้เลือกว่า หากพวกเขายังคงรักษาคุณลักษณะพิเศษเฉพาะตัวไว้ ก็ต้องแยกตัวออกไปใช้ชีวิตเป็นเอกเทศ ไม่ข้องเกี่ยวกับสังคมคนธรรมดา หรือมิฉะนั้นก็ต้องยอมลบล้างพลังพิเศษของพวกเขาแล้วใช้ชีวิตเยี่ยงปุถุชน ผู้นำกลุ่มมนุษย์กลายพันธุ์ที่คิดต่างกันคนละขั้ว</p>
+                    <iframe width="720" height="400" src="https://www.youtube.com/embed/gT2nVJTSPSc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <h2 class="bg-danger text-white"> เรื่องย่อ </h2>
+                    <h4 class="text-white">บทส่งท้ายไตรภาคของ X-MEN จะเป็นองก์ เยียวยา ที่พอเห็นทางลดทอนภัยจากมนุษย์กลายพันธุ์ที่หวังจะพลิกโฉมหน้าประวัติศาสตร์ เป็นครั้งแรกที่เหล่ามนุษย์กลายพันธุ์จะได้เลือกว่า หากพวกเขายังคงรักษาคุณลักษณะพิเศษเฉพาะตัวไว้ ก็ต้องแยกตัวออกไปใช้ชีวิตเป็นเอกเทศ ไม่ข้องเกี่ยวกับสังคมคนธรรมดา หรือมิฉะนั้นก็ต้องยอมลบล้างพลังพิเศษของพวกเขาแล้วใช้ชีวิตเยี่ยงปุถุชน ผู้นำกลุ่มมนุษย์กลายพันธุ์ที่คิดต่างกันคนละขั้ว</h4>
             </div>
         </div>
     </div>
