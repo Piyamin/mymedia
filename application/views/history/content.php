5e32624a6e2b8c318be5f2297c22b6f403568a75
chotiwich <chotiwich6793@gmail.com>
2020-05-06 23:17:57 +0700

    เพิ่มหน้า History

--- application/views/history/content.php
+++ application/views/history/content.php
@@ -1,6 +1,63 @@
 <div class="row">
-    <div class="row">
-        history
+    <div class="col-12">
+        <h2 class="text-white">History</h2>
     </div>
+    <hr>
+
+    <div class="col-12">
+        <hr>
+        <h2 class="text-white">เพิ่งเข้าดูล่าสุด</h2>
+    </div>
+
+    <hr>
+    <div class="col-2">
+        <hr>
+        <a href="http://localhost/mymedia/watch"><img src="<?php echo base_url(); ?>public/images/movie2.jpg" class="img-responsive img-fluid" alt="" /></a>
+        <h4 class="text-center  text-white">Spider-Man 2 (2004) ไอ้แมงมุม 2</h4>
+    </div>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/movie92.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">ฮาวทูทิ้ง (2019)</h4>
+    </div>
+
+
+    <div class="col-12">
+        <hr>
+        <h2 class="text-white">ช่วง 1 สัปดาห์ที่ผ่านมา</h2>
+    </div>
+
+    <hr>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/staffs/staff02.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">X-Men: The Last Stand (2006)</h4>
+    </div>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/movie25.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">ดิว ไปด้วยกันนะ (2019)</h4>
+    </div>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/movie28.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">ละติจูดที่ 6 (2018)</h4>
+    </div>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/movie76.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">เคว้ง (2019)</h4>
+    </div>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/movie86.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">น้องพี่ที่รัก (2018)</h4>
+    </div>
+    <div class="col-2">
+        <hr>
+        <img src="<?php echo base_url('public/images/movie91.jpg') ?>" class="img-responsive img-fluid" />
+        <h4 class="text-center  text-white">คิดถึงวิทยา (2017)</h4>
+    </div>
+
 </div>
 </div>
