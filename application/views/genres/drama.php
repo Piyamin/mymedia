752d715879cb7d66a9f04e33788ae1163de2f401
chotiwich <chotiwich6793@gmail.com>
2020-05-06 17:52:57 +0700

    แก้ไขหนังดราม่าเรียบร้อยแล้ว

--- application/views/genres/drama.php
+++ application/views/genres/drama.php
@@ -21,27 +21,56 @@
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie30.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
     </div>
+    <hr>
+    <hr>
+
     <div class="row">
         <div class="col-2">
-            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie27.jpg" class="img-responsive img-fluid " alt="xman" /></a>
+            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie85.jpg" class="img-responsive img-fluid " alt="xman" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie25.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie86.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie26.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie87.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie28.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie88.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie29.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie89.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie30.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie90.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
     </div>
     <hr>
     <hr>
+
+    <div class="row">
+        <div class="col-2">
+            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie91.jpg" class="img-responsive img-fluid " alt="xman" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie92.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie93.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie94.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie95.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie96.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+    </div>
+    <hr>
+    <hr>
+
+
+
 </div>
 </div>
