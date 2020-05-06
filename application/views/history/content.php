bb7f8e7e2c82293ee1b49d1cf2ddc66d8a964159
Piyamin <piyaminjaima42@gmail.com>
2020-05-06 18:13:53 +0700

    create history

---
+++ application/views/history/content.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="row">
+        history
+    </div>
+</div>
+</div>
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -53,7 +53,7 @@
                                 </a>
                             </li>
                             <li class="nav-item active">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="http://localhost/mymedia/history">
                                     HISTORY<span class="sr-only">(current)</span>
                                 </a>
                             </li>
