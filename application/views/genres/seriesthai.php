39f188df50776a30b2a54e189878e711f2389e51
chotiwich <chotiwich6793@gmail.com>
2020-05-06 17:33:02 +0700

    แก้ไขหน้าซีรีย์ไทยเสร็จแล้ว

--- application/views/genres/seriesthai.php
+++ application/views/genres/seriesthai.php
@@ -21,27 +21,55 @@
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie54.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
     </div>
+    <hr>
+    <hr>
+
     <div class="row">
         <div class="col-2">
-            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie49.jpg" class="img-responsive img-fluid " alt="xman" /></a>
+            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie73.jpg" class="img-responsive img-fluid " alt="xman" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie50.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie74.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie51.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie75.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie52.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie76.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie53.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie77.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie54.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie78.jpg" class="img-responsive img-fluid" alt="" /></a>
         </div>
     </div>
     <hr>
     <hr>
+
+    <div class="row">
+        <div class="col-2">
+            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie79.jpg" class="img-responsive img-fluid " alt="xman" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie80.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie81.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie82.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie83.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+        <div class="col-2">
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie84.jpg" class="img-responsive img-fluid" alt="" /></a>
+        </div>
+    </div>
+    <hr>
+    <hr>
+
+
 </div>
 </div>
