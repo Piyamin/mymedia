5fc2310004bb18d0f9f9206f1dfdc73fc1cda1eb
chotiwich <chotiwich6793@gmail.com>
2020-05-17 22:51:37 +0700

    แก้หน้าดูหนังแล้ว

--- application/views/detail/watch/content.php
+++ application/views/detail/watch/content.php
@@ -1,24 +1,15 @@
 <div class="row">
     <div class="col">
         <div class="row">
-            <div class="col-md-8">
-                <iframe width="750" height="400" src="https://www.youtube.com/embed/V9elBa2LPUY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <h4 class="text-center   text-white">Spider-Man 2 (2004) ไอ้แมงมุม 2</h4>
-
-            </div>
-
-
             <div class="col-md-4">
-                <img src="<?php echo base_url('public/images/staffs/staff03.jpg') ?>" class="img-fluid w-50 mx-auto d-block" />
-                <h4 class="text-center  text-white">Spider-Man 2 (2004) ไอ้แมงมุม 2</h4>
-                <span class="text-warning fa fa-star"></span>
-                <span class="text-warning fa fa-star"></span>
-                <span class="text-warning fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <hr>
-
-                <div class="row">
+                    <img src="<?php echo base_url('public/images/staffs/staff03.jpg') ?>" class="img-fluid w-50 mx-auto d-block" />
+                    <h5 class="text-center bg-danger text-white">Spider-Man 2 (2004)</h5>
+                    <span class="text-warning fa fa-star"></span>
+                    <span class="text-warning fa fa-star"></span>
+                    <span class="text-warning fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <br>
                     <br>
                     <div class="col-md-12">
                         <a href=""><button type="button" class="btn btn-outline-success btn-block"> Save </button></a>
@@ -28,11 +19,14 @@
                     <div class="col-md-12">
                         <a href="http://localhost/mymedia/detail2"><button type="button" class="btn btn-outline-success btn-block">Back</button></a>
                     </div>
-                </div>
+            </div>    
+            <div class="col-md-8">
+                    <iframe width="750" height="400" src="https://www.youtube.com/embed/V9elBa2LPUY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <h4 class="text-center   text-white">Spider-Man 2 (2004) ไอ้แมงมุม 2</h4>
             </div>
-
+            <div class="row">
+                    
+            </div>
         </div>
     </div>
-</div>
-</div>
 </div>
