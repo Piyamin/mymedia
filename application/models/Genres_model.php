fa0f3fd20e1fa2591b5cf12777ffdad864ef56bf
Piyamin <piyaminjaima42@gmail.com>
2020-05-18 02:21:46 +0700

    model

---
+++ application/models/Genres_model.php
@@ -0,0 +1,27 @@
+<?php if (!defined('BASEPATH')) exit('No direct script allowed');
+
+class Genres_model extends CI_Model
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function findOne($condition = [])
+    {
+        if (sizeof($condition) > 0) {
+            $this->mongo_db->where($condition);
+        }
+        $result = $this->mongo_db->getOne('genres');
+        return $result;
+    }
+    public function findAll($condition = [])
+    {
+        if (sizeof($condition) > 0) {
+            $this->mongo_db->where($condition);
+        }
+        $result = $this->mongo_db->get('genres');
+        return $result;
+    }
+}
