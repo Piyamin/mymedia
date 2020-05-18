153340a5900cbf88411e816ff2b45bfdb58d74ac
chotiwich <chotiwich6793@gmail.com>
2020-05-18 20:43:48 +0700

    แก้ไขหน้าดราม่าให้แล้วนะครัช

--- application/views/genres/content.php
+++ application/views/genres/content.php
@@ -1,75 +1,94 @@
 <div class="row">
-    <h2 class="text-white">ดราม่า</h2>
-    <hr>
+    <div class="col-12">
+        <h2 class="text-white">ดราม่า</h2>
+        <hr>
     <div class="row">
         <div class="col-2">
             <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie27.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">7 DAYS <br> (2019)</h4>
         </div>
         <div class="col-2">
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie25.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">ดิวไปด้วยกันนะ <br> (2019)</h4>
         </div>
         <div class="col-2">
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie26.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">รุ่นพี่ <br> (2017)</h4>
         </div>
         <div class="col-2">
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie28.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">ละติจูดที่ 6 <br> (2017)</h4>
         </div>
         <div class="col-2">
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie29.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">แผลเก่า <br> (2016)</h4>
         </div>
         <div class="col-2">
             <a href="#"><img src="<?php echo base_url(); ?>public/images/movie30.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">รักแห่งสยาม <br> (2012)</h4>
         </div>
     </div>
     <br>
     <br>
     <div class="row">
         <div class="col-2">
-            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie85.jpg" class="img-responsive img-fluid " alt="xman" /></a>
+            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie85.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">คู่กรรม <br> (2016)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie86.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie86.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">น้องพี่ที่รัก <br> (2018)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie87.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie87.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">พี่ชาย <br> (2017)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie88.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie88.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">โนราห์ <br> (2018)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie89.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie89.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">Classic again <br> (2018)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie90.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie90.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">Timeline  <br> (2015)</h4>
         </div>
     </div>
-    <hr>
-    <hr>
+    <br>
+    <br>
 
     <div class="row">
         <div class="col-2">
-            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie91.jpg" class="img-responsive img-fluid " alt="xman" /></a>
+            <a href="http://localhost/mymedia/detail"><img src="<?php echo base_url(); ?>public/images/movie91.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">คิดถึงวิทยา  <br> (2017)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie92.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie92.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">ฮาวทูทิ้ง  <br> (2019)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie93.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie93.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">Timeline2  <br> (2015)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie94.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie94.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">รักสารภาพ  <br> (2014)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie95.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie95.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">รัก  <br> (2013)</h4>
         </div>
         <div class="col-2">
-            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie96.jpg" class="img-responsive img-fluid" alt="" /></a>
+            <a href="#"><img src="<?php echo base_url(); ?>public/images/movie96.jpg" class="" alt="" width="160" height="214" /></a>
+            <h4 class=" bg-danger text-center  text-white">ร่าง <br> (2016)</h4>
         </div>
     </div>
-    <hr>
-    <hr>
+    <br>
+    <br>
 
 
-
+    </div>
 </div>
 </div>
